522cb989bef1f39e7c26e48131e697e306cbd241
Ritik Gupta <ritik.gupta@techolution.com>
2025-03-20 15:39:37 +0530

    setting up router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
+import Home from './views/Home.vue'
+import { RouterView } from 'vue-router';
+</script>
 
 <template>
-  <h1>Hello vue js</h1>
+  <Navbar></Navbar>
+<RouterView/>
 </template>
