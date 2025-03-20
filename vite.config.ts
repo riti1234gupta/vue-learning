5870c479bbd5cfc3b56529a2a5a7012f8b6cff7f
Ritik Gupta <ritik.gupta@techolution.com>
2025-03-20 15:39:12 +0530

    adding proxy

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  server: {
+    proxy: {
+      '/api': {
+        // Any request starting with "/api" will be proxied
+        target: 'http://localhost:8000', // Backend URL
+        changeOrigin: true, // Changes the origin of the request
+        rewrite: (path) => path.replace(/^\/api/, ''), // Removes "/api" prefix
+      },
+    },
+  },
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
